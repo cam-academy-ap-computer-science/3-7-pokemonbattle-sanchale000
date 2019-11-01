20d764619531681c5c1dde602ac71596fa19e8ea
sanchale000 <sanchale000@CAM-LAB-28.bgsd.k12.wa.us>
2019-11-01 07:49:08 -0700

    Battlestart done?

--- src/PokemonBattle.java
+++ src/PokemonBattle.java
@@ -1,9 +1,20 @@
 
+/*
+ * 
+ */
+import java.util.*;
 public class PokemonBattle {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String name = battleStart();
+	}
+	
+	public static String battleStart() {
+		System.out.print("Another trainer is issuing a challenge!\nZebstrika appeared.\nWhich pokemon do you choose?");
+		Scanner console = new Scanner(System.in);
+		String name = console.next();
+		System.out.print("You chose " + name + "!\nIt's a pokemon battle between " + name + " and Zebsrika!  Go!");
+		return name;		
 	}
 
 }
