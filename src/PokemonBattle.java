5f3906913685f80544423f3a482ebb4233da1448
sanchale000 <sanchale000@CAM-LAB-28.bgsd.k12.wa.us>
2019-11-04 07:43:44 -0800

    not done

--- src/PokemonBattle.java
+++ src/PokemonBattle.java
@@ -10,11 +10,35 @@ public class PokemonBattle {
 	}
 	
 	public static String battleStart() {
-		System.out.print("Another trainer is issuing a challenge!\nZebstrika appeared.\nWhich pokemon do you choose?");
 		Scanner console = new Scanner(System.in);
+		System.out.print("Another trainer is issuing a challenge!\nZebstrika appeared.\nWhich pokemon do you choose?");
 		String name = console.next();
 		System.out.print("You chose " + name + "!\nIt's a pokemon battle between " + name + " and Zebsrika!  Go!");
 		return name;		
+	}
+	public static String damage(String name) {
+		Scanner console = new Scanner(System.in);
+		System.out.print("Zebstrika used Thunderbolt!\n\nTrainer, What are your " + name + "'s stats?");
+		System.out.print("Level:");
+		double lvl = console.nextDouble();
+		System.out.print("Attack:");
+		double att = console.nextDouble();
+		System.out.print("Defense:");
+		double def = console.nextDouble();
+		System.out.print("Base:");
+		double base = console.nextDouble();
+		System.out.print("STAB:");
+		double stab = console.nextDouble();
+		System.out.print("HP:");
+		double hp = console.nextDouble();
+		double mod = (0.85 + Math.random() * 0.15) * stab; 
+		double dmg = (((2 * lvl + 10)/250) + ((att/def) * base) + 2) * mod;
+		hp = hp - dmg; 
+		System.out.print(name + "sustained " + dmg + "points damge.\nHP, after damage, are now " + hp + ".");
+		return hp;
+		
+		
+		
 	}
 
 }
