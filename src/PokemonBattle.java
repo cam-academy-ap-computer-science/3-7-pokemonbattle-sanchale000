ba12a31f4f9eb8888674c1d392d8d8a94be3b3db
sanchale000 <sanchale000@CAM-LAB-28.bgsd.k12.wa.us>
2019-11-04 07:52:42 -0800

    not done

--- src/PokemonBattle.java
+++ src/PokemonBattle.java
@@ -7,6 +7,7 @@ public class PokemonBattle {
 
 	public static void main(String[] args) {
 		String name = battleStart();
+		double hp = damage(name);
 	}
 	
 	public static String battleStart() {
@@ -16,9 +17,9 @@ public class PokemonBattle {
 		System.out.print("You chose " + name + "!\nIt's a pokemon battle between " + name + " and Zebsrika!  Go!");
 		return name;		
 	}
-	public static String damage(String name) {
+	public static double damage(String name) {
 		Scanner console = new Scanner(System.in);
-		System.out.print("Zebstrika used Thunderbolt!\n\nTrainer, What are your " + name + "'s stats?");
+		System.out.print("Zebstrika used Thunderbolt!\n\nTrainer, What are your " + name + "'s stats?\n");
 		System.out.print("Level:");
 		double lvl = console.nextDouble();
 		System.out.print("Attack:");
@@ -34,7 +35,7 @@ public class PokemonBattle {
 		double mod = (0.85 + Math.random() * 0.15) * stab; 
 		double dmg = (((2 * lvl + 10)/250) + ((att/def) * base) + 2) * mod;
 		hp = hp - dmg; 
-		System.out.print(name + "sustained " + dmg + "points damge.\nHP, after damage, are now " + hp + ".");
+		System.out.print(name + " sustained " + dmg + "points damge.\nHP, after damage, are now " + hp + ".");
 		return hp;
 		
 		
