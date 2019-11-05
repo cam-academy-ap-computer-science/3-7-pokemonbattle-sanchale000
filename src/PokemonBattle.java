5c1e4c04b828e47b26ab9e98e7c77a0e0207c02b
sanchale000 <sanchale000@CAM-LAB-28.bgsd.k12.wa.us>
2019-11-05 07:39:56 -0800

    not done

--- src/PokemonBattle.java
+++ src/PokemonBattle.java
@@ -1,5 +1,16 @@
 
-/*
+/*Calls 3 methods
+ * 	battlestart
+ * 	damage
+ * 	statstable
+ *
+ *	battlestart will
+ *		print text
+ *		accept name
+ *		return name
+ *
+ *	damage will
+ *
  * 
  */
 import java.util.*;
@@ -8,11 +19,12 @@ public class PokemonBattle {
 	public static void main(String[] args) {
 		String name = battleStart();
 		double hp = damage(name);
+		statsTable(name, hp);
 	}
 	
 	public static String battleStart() {
 		Scanner console = new Scanner(System.in);
-		System.out.print("Another trainer is issuing a challenge!\nZebstrika appeared.\nWhich pokemon do you choose?");
+		System.out.print("Another trainer is issuing a challenge!\nZebstrika appeared.\nWhich pokemon do you choose? ");
 		String name = console.next();
 		System.out.print("You chose " + name + "!\nIt's a pokemon battle between " + name + " and Zebsrika!  Go!");
 		return name;		
@@ -20,23 +32,39 @@ public class PokemonBattle {
 	public static double damage(String name) {
 		Scanner console = new Scanner(System.in);
 		System.out.print("Zebstrika used Thunderbolt!\n\nTrainer, What are your " + name + "'s stats?\n");
-		System.out.print("Level:");
+		System.out.print("Level: ");
 		double lvl = console.nextDouble();
-		System.out.print("Attack:");
+		System.out.print("Attack: ");
 		double att = console.nextDouble();
-		System.out.print("Defense:");
+		System.out.print("Defense: ");
 		double def = console.nextDouble();
-		System.out.print("Base:");
-		double base = console.nextDouble();
-		System.out.print("STAB:");
+		System.out.print("Base: ");
+		double bas = console.nextDouble();
+		System.out.print("STAB: ");
 		double stab = console.nextDouble();
-		System.out.print("HP:");
+		System.out.print("HP: ");
 		double hp = console.nextDouble();
-		double mod = (0.85 + Math.random() * 0.15) * stab; 
-		double dmg = (((2 * lvl + 10)/250) + ((att/def) * base) + 2) * mod;
-		hp = hp - dmg; 
-		System.out.print(name + " sustained " + dmg + "points damge.\nHP, after damage, are now " + hp + ".");
+		double mod = (0.85 + (Math.random() * 0.15)) * stab; 
+		double dmg = Math.round(((((2 * lvl) + 10)/250) + ((att/def) * bas) + 2) * mod);
+		double result = hp - dmg; 
+		System.out.print(name + " sustained " + dmg + "points damge.\nHP, after damage, are now " + result + ".\n");
 		return hp;
+		
+		
+		
+	}
+	public static String statsTable(String name, double hp) {
+		System.out.println("\nName   " + name);
+		System.out.println("Level 	40");
+		System.out.println("------------------------------");
+		System.out.println("HP    	" + hp);
+		System.out.println("ATTACK	52");
+		System.out.println("DEFENSE   51");
+		System.out.println("SP. ATK   121");
+		System.out.println("SP. DEF   81");
+		System.out.println("SPEED 	107");
+		System.out.println("-------------------------------");
+		return name;
 		
 		
 		
